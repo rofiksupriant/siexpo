20a18880846295b15c723e0a3c6974b31f7902e3
rofik <rofik.baae@gmail.com>
2020-12-03 09:04:36 +0700

    user view

---
+++ resources/views/layouts/templates/user/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="page-footer font-small blue footer ">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="{{url('/')}}">{{ config('app.name', 'Laravel') }}</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
